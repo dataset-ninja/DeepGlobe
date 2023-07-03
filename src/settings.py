23d800b6eca22ec33277ba6e823ca5a9f438811e
Ars Vladimirov <grokhael@gmail.com>
2023-07-04 01:38:26 +0400

    upd settinga

--- src/settings.py
+++ src/settings.py
@@ -33,7 +33,7 @@ HOMEPAGE_URL: str = (
 )
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 1459606
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/DeepGlobe"
